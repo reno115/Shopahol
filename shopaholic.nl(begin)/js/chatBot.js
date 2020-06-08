fb8ba1c4cda18a54b1e426071255a529654d642d
Buck basten <buckbasten@gmail.com>
2020-06-08 09:48:25 +0200

    1.01

--- shopaholic.nl(begin)/js/chatBot.js
+++ shopaholic.nl(begin)/js/chatBot.js
@@ -1,21 +1,21 @@
 // clickEvent show or hide chat omgeving
 function showMsg() {
-            var msg = document.getElementById('msg'); //pak element bij id 
-            var hide = document.getElementById('hide'); //show or hide element by id 
+            var msg = document.getElementById('msg'); //pak element bij id
+            var hide = document.getElementById('hide'); //show or hide element by id
             msg.style.display = 'inline';
-            hide.style.display = 'none'; //display msg box function 
+            hide.style.display = 'none'; //display msg box function
         }
         function closeMsg() {
-            var msg = document.getElementById('msg'); //pak element bij id 
-            var hide = document.getElementById('hide'); //show or hide element by id 
+            var msg = document.getElementById('msg'); //pak element bij id
+            var hide = document.getElementById('hide'); //show or hide element by id
             msg.style.display = 'none';
-            hide.style.display = 'inline'; //display msg box function 
+            hide.style.display = 'inline'; //display msg box function
         }
 
-// het sturen van text (p)    
+// het sturen van text (p)
 function sentMsg() {
-    var input = document.getElementById("input") //pak element bij id 
-    var div = document.getElementById("msg-sec"); //uitprinten in dit div 
+    var input = document.getElementById("input") //pak element bij id
+    var div = document.getElementById("msg-sec"); //uitprinten in dit div
     var t = document.createElement("p"); //(p) weergeven
     var txt = document.createTextNode(input.value.toLowerCase()); //altijd sturen zonder hoofdletter
     t.appendChild(txt); // Uitprinten user input
@@ -35,13 +35,11 @@ function sentMsg() {
           div.appendChild(ansP);
       }
         else {
-            var ans = document.createTextNode("Sorry, Ik begrijp je even niet. Ik ben namelijk nog niet zo slim, maar dat komt nog wel ;0");
+            var ans = document.createTextNode("Sorry, Ik begrijp je even niet. Voor dit soort vragen kun je onze helpdesk contacteren.");
             var ansP= document.createElement("p");
             ansP.appendChild(ans);
             div.appendChild(ansP);
         }
       //Het schoonmaken van de text veld
-      input.value = "";    
+      input.value = "";
 }
-
-
